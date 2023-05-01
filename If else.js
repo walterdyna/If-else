1526841eb70002832c11d1362b87356a4d587bf5
walterdyna <alexdyna@gmail.com>
2023-05-01 20:10:52 -0300

    Condicionais IF e ELSE

---
+++ If else.js
@@ -0,0 +1,38 @@
+/* ------------------Condicionais IF e ELSE------------------
+
+IF    ----> Usado se uma condição e VERDADEIRA
+ELSE  ----> Usado caso a condição do IF seja FALSA
+
+IF    ----> SE     ----> Se sim faça ----> Execute o certo
+ELSE  ----> SE Não ----> Se não - Não faça IF e Sim Else...
+
+ ---------------------- Condicional IF ---------------------- */
+
+const age = 20
+
+if(age >=18) {
+    console.log('A pessoa é maior de idade')
+}
+
+else {
+    console.log('A pessoa NÃO é maior de idade')
+}
+
+console.log('Terminou de executar meu codigo')
+console.log('----------------------------------------------------')
+
+
+/* ---------------------- Condicional ELSE ---------------------- */
+
+const age1 = 15
+
+if(age1 >=17) {
+    console.log('A pessoa é maior de idade')
+}
+
+else {
+    console.log('A pessoa NÃO é maior de idade')
+}
+
+console.log('Terminou de executar meu codigo')
+console.log('----------------------------------------------------')
---
+++ README.md
@@ -0,0 +1,9 @@
+# CONDICIONAIS IF E ELSE - JavaScript
+
+/* ------------------Condicionais IF e ELSE------------------
+
+IF    ----> Usado se uma condição e VERDADEIRA
+ELSE  ----> Usado caso a condição do IF seja FALSA
+
+IF    ----> SE     ----> Se sim faça ----> Execute o certo
+ELSE  ----> SE Não ----> Se não - Não faça IF e Sim Else...
